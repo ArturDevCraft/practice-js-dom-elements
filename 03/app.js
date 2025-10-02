dcb6e50bb68b49efeaff9dd6ecc6b95d29a22f14
Artur <artur.czerwiak@gmail.com>
2025-10-02 10:30:37 +0200

    Task 03 completed

--- 03/app.js
+++ 03/app.js
@@ -1,14 +1,30 @@
 console.log('DOM');
 
 const buttonSettings = {
-    attr: {
-        className: 'btn',
-        title: 'super button'
-    },
-    css: {
-        border: '1px solid #336699',
-        padding: '5px 20px',
-        color: '#444'
-    },
-    text: 'Click me!',
+	attr: {
+		className: 'btn',
+		title: 'super button',
+	},
+	css: {
+		border: '1px solid #336699',
+		padding: '5px 20px',
+		color: '#444',
+	},
+	text: 'Click me!',
+};
+
+const button = document.createElement('button');
+const buttonParent = document.querySelector('.parent-for-button');
+
+for (const prop in buttonSettings.attr) {
+	button.setAttribute(prop, buttonSettings.attr[prop]);
 }
+
+for (const prop in buttonSettings.css) {
+	console.log(buttonSettings.css[prop]);
+	button.style[prop] = buttonSettings.css[prop];
+}
+
+button.innerText = buttonSettings.text;
+
+buttonParent.appendChild(button);
