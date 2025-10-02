df731f9210266aedc5c61d86f72a92291947ee0e
Artur <artur.czerwiak@gmail.com>
2025-10-02 09:54:14 +0200

    Task 01 completed

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,9 @@
 console.log('DOM');
+
+const commentsItem = document.querySelector(
+	'.comments__item.comments__item--newest'
+);
+
+const dataInfo = commentsItem.querySelectorAll('[data-info]');
+
+console.log(dataInfo.length);
