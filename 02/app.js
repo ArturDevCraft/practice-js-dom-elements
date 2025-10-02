bec02fce6bb5afe77018dede4b9e32d7dcc12017
Artur <artur.czerwiak@gmail.com>
2025-10-02 10:00:37 +0200

    Task 02 completed

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const links = document.querySelectorAll('a[data-url]');
+
+links.forEach((element) => {
+	const dataUrl = element.getAttribute('data-url');
+	element.setAttribute('href', dataUrl);
+});
