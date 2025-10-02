5aea45508e4c9ca547ea6d30891598d78aae65dd
Artur <artur.czerwiak@gmail.com>
2025-10-02 10:43:33 +0200

    Task 04 completed

--- 04/app.js
+++ 04/app.js
@@ -2,7 +2,27 @@ console.log('DOM');
 
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+	{ text: 'start', url: '/' },
+	{ text: 'galeria', url: '/gallery' },
+	{ text: 'kontakt', url: '/contact' },
 ];
+
+const menu = document.createElement('ul');
+
+//bez foreach
+menu.innerHTML = `
+    <li><a href="/">start</a></li>
+    <li><a href="/gallery">galeria</a></li>
+    <li><a href="/contact">kontakt</a></li>`;
+
+//z foreach
+menuItems.forEach((el) => {
+	const li = document.createElement('li');
+	const a = document.createElement('a');
+	a.innerText = el.text;
+	a.setAttribute('href', el.url);
+	li.appendChild(a);
+	menu.appendChild(li);
+});
+
+document.querySelector('nav').appendChild(menu);
